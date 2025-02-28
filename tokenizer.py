ef01c13a1498973724a1fe1630184320c4fe1738
Wasay Saeed <mwasaysaeed@gmail.com>
2025-02-28 02:05:28 -0500

    starting argument parsing

--- tokenizer.py
+++ tokenizer.py
@@ -1,2 +1,18 @@
+import argparse
+from pathlib import Path
+
 def tokenizer():
 
+def main():
+    parser = argparse.ArgumentParser(
+        prog='Tokenizer',
+        description='Converts blog language into series of tokens',
+        usage='%(prog)s [filenames]')
+    
+    args = parser.parse_args()
+
+    target_dir = Path(args.path)
+    
+
+if __name__ == "__main__":
+    main()
